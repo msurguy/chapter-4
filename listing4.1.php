d4684f381798360c6b0f0af819fab89d44718fea
Maks Surguy <msurguy@gmail.com>
2014-03-06 11:13:10 -0800

    first commit

---
+++ listing4.1.php
@@ -0,0 +1,7 @@
+Route::get('login', function () {
+  return '<form action="login" method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password">
+            <input type="submit" value="Login">
+          </form>';
+});
